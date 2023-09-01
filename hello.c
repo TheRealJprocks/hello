f04205bc158e093d2c297d142d223c8e41576645
Jared Ponce <jponce2@huskers.unl.edu>
2023-09-01 16:36:43 -0500

    Added my name

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Jared Ponce
+ *         jponce2@huskers.unl.edu
+ * Date: 2023/09/01
+ *
+ * My Name and this is a program that runs my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jared Ponce\n");
+
+
+  return 0;
+}
